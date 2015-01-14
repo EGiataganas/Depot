a6fd0823afc3caf23d0f8ab9b2582103c28234be
Evangelos Giataganas <e.giataganas@gmail.com>
2015-01-14 13:59:44 +0000

    Impement validation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
   PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"]
+  validates :name, :address, :email, presence: true
+  validates :pay_tupe, inclusion: PAYMENT_TYPES
 end
