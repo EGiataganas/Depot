df9e3f5ed11b424da577f929da76b6cd98c99ea9
Evangelos Giataganas <e.giataganas@gmail.com>
2015-01-15 17:48:06 +0000

    Implement logging admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114111129) do
+ActiveRecord::Schema.define(version: 20150115105144) do
 
   create_table "cards", force: true do |t|
     t.datetime "created_at"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20150114111129) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
